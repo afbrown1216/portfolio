0f72537e0085ce61c6545c44d5e1e4d06ec1eaea
Phantom <fay.ashbro@gmail.com>
2020-11-04 23:39:30 -0500

    changed the link for the About page. unlinked the logo a tag

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
         <div>
             <nav>
                 <div className="nav-wrapper">
-                <a href="#" className="brand-logo">Logo</a>
+                <a className="brand-logo">Logo</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to="/" >About</Link></li>
                         <li><Link to="/portfolio" >Portfolio</Link></li>
-                        <li><Link to="/about" >About</Link></li>
                         <li><Link to="/contact" >Contact</Link></li>
                     </ul>
                 </div>
