e7209853414605efd4d47fb8164b7d024e91f03e
Phantom <fay.ashbro@gmail.com>
2020-11-04 23:32:11 -0500

    brought in react-router to create paths for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react"; 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Nav from "./Components/Navbar/Navbar";
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1> Hello World </h1>
-    </div>
+   <Router> 
+     <div> 
+       <Nav /> 
+       <Switch> 
+         <Route exact path="/" component={About} />
+         <Route exact path="/contact" component={Contact} />
+         <Route exact path="/portfolio" component={Portfolio} />
+         <Route  component={NoMatch} />
+       </Switch>
+     </div>
+   </Router>
   );
 }
 
