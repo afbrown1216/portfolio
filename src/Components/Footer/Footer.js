e25525ea871047a3fb01a6357afcd554f2414673
Phantom <fay.ashbro@gmail.com>
2020-11-04 23:50:00 -0500

    added materlize footer code, brought in sticky footer css

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.js";
+
+const Footer = () => {
+    return (
+        <footer className="page-footer">
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2020 Ashley Brown
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
