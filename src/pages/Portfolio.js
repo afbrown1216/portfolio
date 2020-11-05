f70ee2e5ec5934ae0221ef42a694e1caf66e8b15
Phantom <fay.ashbro@gmail.com>
2020-11-05 00:51:10 -0500

    created a state passed  props to the project component, displays each project with details

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Project from "../Components/Project/Project";
+import projects from "../Projects.json";
 
-const Portfolio = () => {
-    return (
-        <div>
-            <h1> Portfolio page </h1>
-        </div>
-    );
-};
+class Portfolio extends Component {
+    state ={
+        projects
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="center-align"> Projects </h1>
+                <div className="row">
+                    {this.state.projects.map(project => (
+                        <Project
+                            image={project.image} 
+                            name={project.name}
+                            links={project.links}
+                            des={project.description}
+                        />
+                    ))}
+                </div>
+               
+                
+            </div>
+        );
+    }
+}
 
 export default Portfolio;
