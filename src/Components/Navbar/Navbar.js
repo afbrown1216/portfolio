d2b1e06ef9d15acd0bbfd7ebe682e2b5247ee841
Phantom <fay.ashbro@gmail.com>
2020-11-04 23:33:23 -0500

    brought in link from router added materilize navbar code

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom"; 
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Logo</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to="/portfolio" >Portfolio</Link></li>
+                        <li><Link to="/about" >About</Link></li>
+                        <li><Link to="/contact" >Contact</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            
+        </div>
+    );
+};
+
+export default Navbar;
