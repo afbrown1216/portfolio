35fae6ed0f51826a91be9b01ce5c9d92b2177260
Phantom <fay.ashbro@gmail.com>
2020-11-28 19:30:34 -0500

    Fixing the link to redirect

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Link } from "react-router-dom"; 
 
 const Project = (props) => {
+
+    // const clickDeployed = () => {
+
+    // }
+
     return (
        
             <div class="col s6">
                 <div className="card medium hoverable">
                     <div className="card-image waves-effect waves-block waves-light">
-                        <img className="activator" style={imgStyle} src={props.image} alt="Project preview image"/>
+                        <img className="activator" style={imgStyle} src={props.image} alt="Project preview"/>
                     </div>
                     <div className="card-content">
-                        <span className="card-title activator grey-text text-darken-4">{props.name} <i className="material-icons right">More Info</i></span>
-                        <p><a href="{props.links.deployedLink}">Deployed Link</a></p>
-                        <p><Link to="{props.links.gitHubLink}">GitHub Link</Link></p>
+                        <span className="card-title activator grey-text text-darken-4">{props.name} <i className="material-icons right"> more_vert</i></span>
+                        <p><Link target="_blank" to={{pathname: props.links.deployedLink}}> Deployed Link </Link></p>
+                        <p><Link to={{ pathname: props.links.gitHubLink}} target="_blank"> GitHub Link </Link></p>
                     </div>
                     <div className="card-reveal">
                         <span className="card-title grey-text text-darken-4"> {props.name} <i className="material-icons right">close</i></span>
